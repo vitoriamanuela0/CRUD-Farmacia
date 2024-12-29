660290ef092365eed7c49784d0fe102f2f9aa1f0
vitoriamanuela0 <contatodavitoria@gmail.com>
2024-12-29 14:19:40 -0300

    CRUD-Farmácia 💊

--- src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
+++ src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
@@ -88,6 +88,20 @@ public class ProdutoController {
         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
     }
 
+    @PutMapping
+    public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
+        if (produtoRepository.existsById(produto.getId())) {
+
+            if (categoriaRepository.existsById(produto.getCategoria().getId()))
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(produtoRepository.save(produto));
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
